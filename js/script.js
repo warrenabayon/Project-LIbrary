2eb581398df842f48f63edc4260a695d5478728c
Waron <warrenabayon@gmail.com>
2024-05-18 05:07:48 +0800

    no of pages redesigned and added delete icon

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function Book(title, author, pagesNo, isRead) {
 }
 
 Book.prototype.info = function() {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  return `${this.title} by ${this.author}, ${this.pagesNo} pages, ${this.isRead}`;
 }
 
 //add dummy content
@@ -52,6 +52,7 @@ function showBooks(bookArr) {
     //create the elements
     const bookItems = document.createElement('article');
     const bookCover = document.createElement('div');
+    const bookBtnDelete = document.createElement('button');
     const bookDetails = document.createElement('div');
     const bookTitle = document.createElement('h3');
     const bookAuthor = document.createElement('h4');
@@ -70,10 +71,16 @@ function showBooks(bookArr) {
                                         
                                         </span>`;
     bookPagesNo.innerHTML = `${val.pagesNo} Pages`;
+    bookBtnDelete.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14 10V17M10 10V17M6 6V17.8C6 18.9201 6 19.4798 6.21799 19.9076C6.40973 20.2839 6.71547 20.5905 7.0918 20.7822C7.5192 21 8.07899 21 9.19691 21H14.8031C15.921 21 16.48 21 16.9074 20.7822C17.2837 20.5905 17.5905 20.2839 17.7822 19.9076C18 19.4802 18 18.921 18 17.8031V6M6 6H8M6 6H4M8 6H16M8 6C8 5.06812 8 4.60241 8.15224 4.23486C8.35523 3.74481 8.74432 3.35523 9.23438 3.15224C9.60192 3 10.0681 3 11 3H13C13.9319 3 14.3978 3 14.7654 3.15224C15.2554 3.35523 15.6447 3.74481 15.8477 4.23486C15.9999 4.6024 16 5.06812 16 6M16 6H18M18 6H20" stroke="#D3C8B1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `;
   
     //add classes
     bookItems.classList.add('bookitem');
-    bookCover.classList.add('bookcover')
+    bookCover.classList.add('bookcover', 'dflex');
+    bookPagesNo.classList.add('bookpagesno')
+    bookBtnDelete.classList.add('bookDelete');
     bookDetails.classList.add('bookdetails');
     bookTitle.classList.add('title');
     bookRow.classList.add("row", "dflex", 'align-fl-cen', 'just-con-sp-bet');
@@ -92,16 +99,20 @@ function showBooks(bookArr) {
     bookRow.appendChild(bookRowColLeft);
     bookRowColLeft.appendChild(bookBtnStatus);
     bookRow.appendChild(bookRowColRight);
-    bookRowColRight.appendChild(bookPagesNo)
+    bookCover.appendChild(bookPagesNo)
+    bookRowColRight.appendChild(bookBtnDelete);
     
     //change status of read to continue
-    console.log(bookBtnStatus.getAttribute('class'));
+    // console.log(bookBtnStatus.getAttribute('class'));
     bookBtnStatus.addEventListener('click',(e) => {
-      if(bookBtnStatus.textContent.includes('Read')) {
-        bookBtnStatus.textContent = 'Continue';
+      if(val.isRead.includes('Read')) {//check the value if book read status
+        val.isRead = 'Continue';
       } else {
-        bookBtnStatus.textContent = 'Read Now';
+        val.isRead = 'Read Now';
       }
+      //reload
+      showBooks(bookArr);
+      
     })
 
   }); 
